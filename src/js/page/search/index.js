5d04f40f33426de5c51902a858b45d76776cd268
Gmyth <332087251@qq.com>
2016-10-28 18:45:54 -0400

    update api for sign up

--- src/js/page/search/index.js
+++ src/js/page/search/index.js
@@ -45,7 +45,9 @@ define(function(require, exports, module){
                 "selllevel": input_select_level,
                 "check_box_id1": input_open==undefined?"0":"1",
                 "txtstarttime": input_starttime,
-                "txtendtime": input_endtime
+                "txtendtime": input_endtime,
+                "selstart": parseInt(input_select_start),
+                "selend": parseInt(input_select_end),
             }
             var success = function(data) {
                 // callback
